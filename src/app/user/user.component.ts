297f8f0a2e5a71a1bb6a4d5477ef5adffe08a727
Sebastian Ebel <sebastianebel@hotmail.de>
2021-12-11 06:41:02 +0100

    read db connected

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {MatDialog} from '@angular/material/dialog';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { MatDialog } from '@angular/material/dialog';
 import { User } from 'src/models/user.class';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 
@@ -11,14 +12,21 @@ import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.compo
 export class UserComponent implements OnInit {
 
   user = new User();
-  
-  
-  constructor(public dialog: MatDialog) { }
+  allUsers = [];
+
+  constructor(public dialog: MatDialog, private firestore: AngularFirestore) { }
 
   ngOnInit(): void {
+    this.firestore
+      .collection('users')
+      .valueChanges()
+      .subscribe((changes: any) => {
+        console.log('received changes from DB', changes);
+        this.allUsers = changes;
+      });
   }
 
-  openDialog(){
+  openDialog() {
     this.dialog.open(DialogAddUserComponent);
   }
 }
