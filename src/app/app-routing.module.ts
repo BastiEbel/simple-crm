40f530c27e0c30d4af2ab9ae47708d80780d4728
Sebastian Ebel <sebastianebel@hotmail.de>
2021-12-18 07:52:15 +0100

    login component added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { UserComponent } from './user/user.component';
 import { UserDetailComponent } from './user-detail/user-detail.component';
+import { LoginUserComponent } from './login-user/login-user.component';
 
 const routes: Routes = [
   {path: '', component: DashboardComponent},
+  {path: 'login', component: LoginUserComponent},
   {path: 'dashboard', component: DashboardComponent},
   {path: 'user', component: UserComponent},
   {path: 'user/:id', component: UserDetailComponent}
---
+++ src/app/login-user/login-user.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login-user',
+  templateUrl: './login-user.component.html',
+  styleUrls: ['./login-user.component.scss']
+})
+export class LoginUserComponent implements OnInit {
+
+  hide = true;
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
